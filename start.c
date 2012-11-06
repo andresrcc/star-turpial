4e4dbdb28c9d4436da59363ab34a7be6c58bc693
moalover <moalover@yahoo.es>
2012-11-06 10:06:02 -0430

    arreglado el chorizero feo e codigo

--- start.c
+++ start.c
@@ -28,26 +28,33 @@ struct objeto{
         vector3D acc; //vectos aceleracion
 };typedef struct objeto figura;
 
+//registro que representa los datos reelevantes para la partida
+struct gameData{
+	int time_a;//tiempo que ha pasado desde el ultimo calculo de FPS
+	int time_d;//tiempo que ha pasado desde que se inicio el calculo del nuevo FPS
+	int frames;//conteo de frames
+	float fps;//FPS
+	char fps_str[50];//representacion en string el FPS
+	int puntos;//puntaje
+	char puntaje[20];//representacion en string del puntaje
+	float rate;//taza con la desarrolla el juego
+	float rate_store;//variable en donde se guarda la taza cuando el juego es pausado
+	char rate_str[50];//representacion en string de la taza
+};typedef struct gameData datosJuego;
+
 
 int movement_pressed_x = 0; //1. hay una tecla presionada que afecta el movimiento en el eje x, 2.'1' es falso
 int movement_pressed_y = 0; //1. hay una tecla presionada que afecta el movimiento en el eje y, 2.'1' es falso
 int movement_pressed_z = 0; //1. hay una tecla presionada que afecta el movimiento en el eje z, 2.'1' es falso
 GLuint texture; //textura de fondo
-int frames = 0;//conteo de frames
-int time_a = 0.0;
-float rate = 1.0;
-float rate_store = 0.0;
-char rate_str[50];
-int time_d = 0.0;
-float fps = 0.0;
-char FPS[50];
-int puntos = 0;
-char puntaje[20];
-figura objetos[3]; //arreglo con todos los objetos, no hay razon alguna para este tamano, se arreglara cuando sepamos el numero maximo de objetos
-figura blancos[5];
-figura anillos[5];
-//objetos[0] : Nave
-//objetos[10] : bala
+datosJuego juego; //datos relevantes para el juego
+figura objetos[2]; //objetos[0]: nave, objetos[1]: laser
+figura blancos[5]; //arreglo con todos los blanco del escenario
+figura anillos[5]; //arreglo con todos los anillos del escenario
+
+//Colores de luces
+GLfloat lData4[] = {-1,1,0,0.0};
+
 
 
 /**
@@ -66,38 +73,46 @@ GLMmodel *modelo_ANILLO = NULL;
 GLUquadricObj *quadratic;
 
 
+/*
+ * Funcion que se encarga de imprimir en la posicion data en panatalla el string pasado como argumento
+ */
 void print_pantalla(float x, float y, char *string)
 {
-  glPushMatrix();
- // glColor3f( 0, 1, 0 );
-  glRasterPos3f(x, y, objetos[0].pos.z);
-  int len, i;
-  len = (int) strlen(string);
-  for (i = 0; i < len; i++) {
-    glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, string[i]);
-  }
-  glPopMatrix();
+	glPushMatrix();
+		glRasterPos3f(x, y, objetos[0].pos.z+2);
+		int len, i;
+		len = (int) strlen(string);
+		for (i = 0; i < len; i++) {
+			glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, string[i]);
+		}
+	glPopMatrix();
 }
 
+/*
+ * Funicion que cambia el estado de una figura dependiendo de si esta o no en al area de interes para el juego
+ * (area de interes: volumen contenido ente el plano z=nave.z y el plano z=nave.z-30)
+ */
+void screen_time(figura *fig){
+	float guard = (*fig).pos.z-objetos[0].pos.z;
+	if(guard < -30 || guard > 0){
+		(*fig).state = 0;
+	}else{
+		(*fig).state = 1;
+	}
+}
 
 
 /*
  * Esta funcion recibe una figura y calcula su posicion y velocidad respecto al tiempo
  */
 void movimiento(figura *fig){
-	//Si la figura sale del volumen de proyeccion no se calcula nada
-	float guard = (*fig).pos.z-objetos[0].pos.z;
-	if(guard < -30 || guard > 0){
-		(*fig).state = 0;
-		return;
-	}else{
-		(*fig).state = 1;
-	}
 	//Se calcula nTime, el tiempo que ha pasado desde la ultima llamada a esta funcion con este parametro fig
 	float nTime = 0-(*fig).time;
 	(*fig).time = glutGet(GLUT_ELAPSED_TIME)*0.001;
         nTime += (*fig).time;
-	nTime *= rate;
+	nTime *= juego.rate;
+
+	//VELOCIDAD v = t*a + v0
         //se calcula el cambio de velocidad debido a la aceleracion
         //para cada caso si la velocidad es menor que el maximo permitido se recalcula,
         //de lo contrario se usa la velocidad maxima.
@@ -130,7 +145,7 @@ void movimiento(figura *fig){
 	}
 
 
-
+	//POSICION x = t*v + x0
         //se calcula el cambio en la posicion debido a la velocidad
         (*fig).pos.x += (*fig).vel.x*nTime;
         (*fig).pos.y += (*fig).vel.y*nTime;
@@ -168,7 +183,11 @@ void movimiento_nave(figura *fig){
                         (*fig).acc.y = (*fig).inertia;
                 }
         }
+	
+	//se llama a la funcion movimiento para que haga el resto de los calculos de movimiento
 	movimiento(fig);
+
+	//se hace un loop del espacio en pantalla
 	if((*fig).pos.x>7.5){
 		(*fig).pos.x -= 15;
 	}
@@ -183,20 +202,18 @@ void movimiento_nave(figura *fig){
 	}
 }
 
-
+/*
+ * Funcino que dibuja un blanco en la posicion dada
+ */
 void dibujar_blanco(float x, float y, float z){
   glPushMatrix();
-  //glClear(GL_COLOR_BUFFER_BIT);
-  //glLoadIdentity();
-  glTranslatef(x,y,z);
-  //glColor3f(0.2,0.1,0.4);
-  //gluDisk(quadratic,0.5,3.5,32,32);
-  glutSolidTorus(0.05,0.5,20,20);
+    glLoadIdentity();
+    glTranslatef(x,y,z);
+    glutSolidTorus(0.05,0.5,20,20);
   glPopMatrix();
 
   //glFlush();
 }
-
 
 
 /*
@@ -251,46 +268,65 @@ void dibujar_cubo(GLfloat anch, GLfloat alt, GLfloat prof){
 	glEnd();
 }
 
+void dibujar_nave(){
+	glPushMatrix();
+  		glTranslatef(objetos[0].pos.x,objetos[0].pos.y,objetos[0].pos.z);
+  		glRotatef(-180.0,0,1,0);
+  		glmUnitize(modelo_TURPIAL);
+ 		glmFacetNormals(modelo_TURPIAL);
+  		glmVertexNormals(modelo_TURPIAL, 90.0);
+  		glEnable(GL_COLOR_MATERIAL);
+  		glmDraw(modelo_TURPIAL, GLM_SMOOTH | GLM_MATERIAL);
+ 	glPopMatrix();
+
+}
+
+void dibujar_lasers(){
+	int i;
+	for (i =1 ; i < 2 ; i++){
+    	if(objetos[i].state != 0){
+      		glPushMatrix();
+      			GLfloat red[] = {1.0f,0.0f,0.0f,1.0f};
+      			GLfloat black[] = {0.0f,0.0f,0.0f,1.0f};
+      			GLfloat diffuse[] = {0.8f,0.8f,0.8f,1.0f};
+      			glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION , red);
+      			glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE , red);
+      			glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR , red);
+      			glTranslatef(objetos[i].pos.x,objetos[i].pos.y,objetos[i].pos.z);
+      			dibujar_cubo(0.1,0.5,0.1);
+      			glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION , black);
+      			glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR , black);
+      			glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE , diffuse);
+      		glPopMatrix();
+    	}
+  	}
+}
+
+
+
+
 /*
  * Funcion que dibuja a todos los objetos que se veran en la escena
  */
 void dibujar_objetos(){
   int i = 0;
-  glPushMatrix();
-	//Se dibuja la nave
-  	glTranslatef(objetos[0].pos.x,objetos[0].pos.y,objetos[0].pos.z);
-  	glRotatef(-180.0,0,1,0);
-  //	glRotatef(90.0,1,0,0); 
-  	glmUnitize(modelo_TURPIAL);
- 	glmFacetNormals(modelo_TURPIAL);
-  	glmVertexNormals(modelo_TURPIAL, 90.0);
-  	glEnable(GL_COLOR_MATERIAL);
-  	glmDraw(modelo_TURPIAL, GLM_SMOOTH | GLM_MATERIAL);
-  glPopMatrix();
-  //Se sibujan los lasers
-  for (i =1 ; i < 2 ; i++){
-    if(objetos[i].state != 0){
-      glPushMatrix();
-      GLfloat red[] = {1.0f,0.0f,0.0f,1.0f};
-      GLfloat black[] = {0.0f,0.0f,0.0f,1.0f};
-      GLfloat diffuse[] = {0.8f,0.8f,0.8f,1.0f};
-      glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION , red);
-      glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE , red);
-      glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR , red);
-      glTranslatef(objetos[i].pos.x,objetos[i].pos.y,objetos[i].pos.z);
-      dibujar_cubo(0.1,0.5,0.1);
-      glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION , black);
-      glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR , black);
-      glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE , diffuse);
-      glPopMatrix();
+  //Se dibuja la nave
+  dibujar_nave();
+  //Se dibujan los lasers
+  dibujar_lasers();
+
+  //para cada anillo y blanco se evalua si va a estar en pantalla
+  for (i = 0 ; i < 5 ; i++){
+      screen_time(&anillos[i]);
+      screen_time(&blancos[i]);
+  }
+ 
+  //se dibujan los toros
+  for (i = 0 ; i<5 ; i++){
+    if(anillos[i].state != 0){
+      dibujar_blanco(anillos[i].pos.x,anillos[i].pos.y,anillos[i].pos.z);  
     }
   }
-  //se dibujan los toros
-   for (i = 0 ; i<5 ; i++){
-    if(anillos[i].state != 0){
-    dibujar_blanco(anillos[i].pos.x,anillos[i].pos.y,anillos[i].pos.z);  
-     }
-   }
 
   //se le indica a glut que la escena debe ser redibujada
   glutPostRedisplay();
@@ -324,7 +360,10 @@ GLuint loadBMP(char * imagepath){
                 dataPos = 54;
         }
         data = (unsigned char*)malloc(imageSize*sizeof(unsigned char));
-        fread(data,1,imageSize,file);
+        if (!fread(data,1,imageSize,file)){
+		fclose(file);
+		return -1;
+	}
         fclose(file);
         GLuint textureID;
         glGenTextures(1,&textureID);
@@ -357,11 +396,34 @@ void drawBackground() {
 
 
 
+}
+
+/*
+ * Funcion que hace todos los calculos de movimiento en el mundo
+ */
+void movimiento_global(){
+	//se calcula el movimiento de la nave
+	movimiento_nave(&objetos[0]);
+  
+	//se calcula el movimiento de los lasers
+	screen_time(&objetos[1]);
+	if(objetos[1].state != 0){
+		movimiento(&objetos[1]);
 	}
+}
 
 
-
-
+/*
+ * Funcion que imprime todos los datos necesarios para el juego en pantalla
+ */
+void print_pantalla_global(){
+	print_pantalla(objetos[0].pos.x,objetos[0].pos.y,".posicion");
+	sprintf(juego.rate_str,"Rate : %f",juego.rate);
+	print_pantalla(0.0,3.5,juego.rate_str); 
+	print_pantalla(-6.0,-4.0,juego.fps_str);//Se imprime el FPS
+	sprintf(juego.puntaje,"Puntos: %d",juego.puntos);
+	print_pantalla(4.0,-4.0,juego.puntaje);//Se imprime el puntaje
+}
 
 
 
@@ -371,37 +433,40 @@ void drawBackground() {
 void display(){
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
-  //se calcula el movimiento de la nave
-    //se calcula el movimiento de los demas objetos
-  int i;
-  for (i = 0 ; i < 5 ; i++){
-      movimiento(&anillos[i]);
-      movimiento(&blancos[i]);
-  }
-  movimiento_nave(&objetos[0]);
+ 
+  //calculos de movimiento antes de dibujar la escena
+  movimiento_global(); 
+  
 
-  movimiento(&objetos[1]);
-  glClearColor(0.0,0.0,0.0,0.0);
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-  GLfloat lData4[] = {-1,1,0,0.0};
-  glLightfv(GL_LIGHT0, GL_POSITION, lData4);
-  //Camara
+
+  //se pone la camara siempre detras de la nave
   gluLookAt(0.0, //Coordenada X
 	    0.0, //Coordenada Y
 	    objetos[0].pos.z+15.0, //Coordenada Z
 	    0.0, 0.0, objetos[0].pos.z, //Posicion Inicial Camara
 	    0.0, 1, 0.0 //Vector UP En este caso, Z
-	    );
+  );
+
+
+  //dibujando el display 
+  glClearColor(0.0,0.0,0.0,0.0);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  
+  //cambiamos la posicion de la luz GL_LIGHT0
+  glLightfv(GL_LIGHT0, GL_POSITION, lData4);
+
+
+  //se dibuja el background
   glDisable(GL_LIGHTING);
   drawBackground();
   glEnable(GL_LIGHTING);
+
+  //se dibujan todos los objetos
   dibujar_objetos();
-  print_pantalla(objetos[0].pos.x,objetos[0].pos.y,".posicion");
-  sprintf(rate_str,"Rate : %f",rate);
-  print_pantalla(0,4,rate_str); 
-  print_pantalla(-7,-4,FPS);//Se imprime el FPS
-  sprintf(puntaje,"Puntos: %d",puntos);
-  print_pantalla(4,-4,puntaje);//Se imprime el puntaje
+
+  //se imprimen todos los datos en pantalla
+  print_pantalla_global();
+
   glutSwapBuffers();
 }
 
@@ -438,39 +503,43 @@ void teclado (unsigned char tecla, int x, int y){
   float acc = objetos[0].engineAcc;
   switch(tecla){
      case 27:
-       exit(0);
+	exit(0);
      case 'w':
+	//arriba
 	movement_pressed_y = 1;
-       objetos[0].acc.y = acc;
-       break;
+        objetos[0].acc.y = acc;
+        break;
      case 'a':
-       //izquierda: 
+        //izquierda: 
 	movement_pressed_x = 1;
-       objetos[0].acc.x = -acc;
-       break;
+        objetos[0].acc.x = -acc;
+        break;
      case 's':
 	//abajo:
 	movement_pressed_y = 1;
-       objetos[0].acc.y = -acc;
-       break;
+        objetos[0].acc.y = -acc;
+        break;
      case 'd':
 	//derecha:
 	movement_pressed_x = 1;
-       objetos[0].acc.x = acc;
-       break;
+        objetos[0].acc.x = acc;
+        break;
      case 'p':
-       if (rate == 0){
-         rate = rate_store;
-       }else{
-         rate_store = rate;
-         rate = 0;
-       }
-       break;
+	//pausa
+        if (juego.rate == 0){
+        	juego.rate = juego.rate_store;
+        }else{
+        	juego.rate_store = juego.rate;
+        	juego.rate = 0;
+        }
+        break;
      case '+':
-       rate *= 2;
-       break;
+	//aumento de rate
+        juego.rate *= 2;
+        break;
      case '-':
-       rate /= 2;
+	//disminucion de rate
+        juego.rate /= 2;
   }
 }
 
@@ -481,15 +550,19 @@ void teclado (unsigned char tecla, int x, int y){
 void teclado_up (unsigned char tecla, int x, int y){
   switch(tecla){
 	case 'a':
+		//izquierda
 		movement_pressed_x = 0;
 		break;
 	case 's':
+		//abajo
 		movement_pressed_y = 0;
 		break;
 	case 'w':
+		//arriba
 		movement_pressed_y = 0;
 		break;
 	case 'd':
+		//derecha
 		movement_pressed_x = 0;
 		break;
 	}
@@ -570,7 +643,7 @@ void init(){
         anillos[0].engineAcc = 0.0;
 
 
-
+	juego.rate = 1.0;
 
 
 	texture = loadBMP("stars.bmp");
@@ -592,16 +665,18 @@ GLvoid mouse_action(GLint button, GLint state, GLint x, GLint y){
         }
 }
 
-
+/*
+ * Funcion que calcula los cuadros por segundo de la aplicacion
+ */
 void calcular_fps(){
-	frames++;
-	time_d = glutGet(GLUT_ELAPSED_TIME);
-	int nTime = time_d - time_a;
+	juego.frames++;
+	juego.time_d = glutGet(GLUT_ELAPSED_TIME);
+	int nTime = juego.time_d - juego.time_a;
 	if(nTime > 1000){
-		fps = frames / (nTime / 1000.0f);
-		sprintf(FPS,"%f FPS",fps);
-		time_a = time_d;
-		frames = 0;
+		juego.fps = juego.frames / (nTime / 1000.0f);
+		sprintf(juego.fps_str,"%f FPS",juego.fps);
+		juego.time_a = juego.time_d;
+		juego.frames = 0;
 	} 	
 
 }
